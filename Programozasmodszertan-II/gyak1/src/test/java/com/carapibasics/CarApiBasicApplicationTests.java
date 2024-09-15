e446faf5f9e20c471228f23406004d555448bc91
Nvirs20 <115690291+Nvirs@users.noreply.github.com>
2024-09-15 12:23:37 +0200

    Programozásmodszertan II. első anyag feltöltése

---
+++ Programozasmodszertan-II/gyak1/src/test/java/com/carapibasics/CarApiBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carapibasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApiBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
